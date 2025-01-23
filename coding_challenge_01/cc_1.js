1087cc1fec3bfbbf341cf9fa03df6848274e6c71
Vaclav Ruzicka <vaclavruzicka@usf.edu>
2025-01-22 23:23:54 -0500

    Task 1 - Employee Information

---
+++ coding_challenge_01/cc_1.js
@@ -0,0 +1,13 @@
+//Task 1 - Variables
+let employeeName = "John Smith"
+const employeeID = 34128
+var isActive = true
+
+console.log("Employee Name:", employeeName)
+console.log("Type of Data:", typeof employeeName)
+
+console.log("Employee ID:", employeeID)
+console.log("Type of Data:", employeeID)
+
+console.log("Is this user active?", isActive)
+console.log("Type of Data:", typeof isActive)
