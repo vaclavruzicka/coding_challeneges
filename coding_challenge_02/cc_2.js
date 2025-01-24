36be66ad85a0567fbcdb489c0701b169ecf98a49
Vaclav Ruzicka <vaclavruzicka@usf.edu>
2025-01-24 01:27:34 -0500

    Task 1 - Store Inventory

---
+++ coding_challenge_02/cc_2.js
@@ -0,0 +1,12 @@
+//Task 1 - Working with Arrays
+let products = ["Laptop", "Mouse", "Keyboard", "Monitor", "Mousepad"]
+console.log(products)
+
+products.push("Headphones")
+console.log(products)
+
+products.pop()
+console.log(products)
+
+
+
