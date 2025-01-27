498efb8c755cf7507a22b83e68de9c1c34a4f11d
Vaclav Ruzicka <vaclavruzicka@usf.edu>
2025-01-26 19:25:49 -0500

    Task 2 - Student Scores

--- coding_challenge_02/cc_2.js
+++ coding_challenge_02/cc_2.js
@@ -1,12 +1,23 @@
 //Task 1 - Working with Arrays
 let products = ["Laptop", "Mouse", "Keyboard", "Monitor", "Mousepad"]
-console.log(products)
+console.log("Products:", products)
 
 products.push("Headphones")
-console.log(products)
+console.log("Products:", products)
 
 products.pop()
-console.log(products)
+console.log("Products:", products)
+
+//Task 2 - Accesing and Modifying Arrays
+let scores = [5, 10, 15, 20, 25]
+console.log("Scores:", scores)
+scores[1] = 11
+console.log("Scores:", scores)
+let total = scores.reduce((sum , score) => sum + score, 0)
+let length = scores.length
+let average = total / length
+console.log("Average Score:", average)
+
 
 
 
